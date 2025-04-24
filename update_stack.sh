270eec56ad26fd7a2ab1529abee91992419133bc
Dante <dante@kubriksoftware.com>
2025-04-24 15:26:53 -0300

    Script improvements

--- update_stack.sh
+++ update_stack.sh
@@ -1,25 +1,15 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-if ! command -v jq &> /dev/null; then
-    echo "Error: jq is not installed. Please install it using:"
-    echo "  sudo apt-get install jq"
-    exit 1
-fi
+set -euo pipefail
 
-if ! command -v sed &> /dev/null; then
-  echo "Error: sed is not installed. Please install it using:"
-  echo "   sudo apt-get install sed"
-  exit 1
-fi
-
-latest_release_tag=$(curl -s https://api.github.com/repos/10Pines/pdepreludat/releases/latest | jq -r '.tag_name')
+latest_release_tag=$(curl -s "https://api.github.com/repos/10Pines/pdepreludat/releases/latest" | jq -r '.tag_name')
 
 if [ -z "$latest_release_tag" ]; then
     echo "Error: Failed to extract latest release tag"
     exit 1
 fi
 
-latest_commit=$(curl -s https://api.github.com/repos/10Pines/pdepreludat/git/ref/tags/${latest_release_tag} | jq -r '.object.sha')
+latest_commit=$(curl -s "https://api.github.com/repos/10Pines/pdepreludat/git/ref/tags/${latest_release_tag}" | jq -r '.object.sha')
 
 if [ -z "$latest_commit" ]; then
     echo "Error: Failed to extract commit SHA"
@@ -34,10 +24,7 @@ if [ ! -f "$FILE" ]; then
 fi
 
 sed -i "s/commit: *.*/commit: $latest_commit/g" "$FILE"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to update commit SHA in $FILE"
-  exit 1
-fi
+
 echo "Updated commit SHA in $FILE"
 
 stack_yaml_data=$(curl -H "Accept: application/vnd.github.raw" https://api.github.com/repos/10Pines/pdepreludat/contents/stack.yaml)
@@ -57,9 +44,7 @@ fi
 echo "Fetched resolver: $resolver"
 
 sed -i "s/resolver: *.*/$resolver/g" "$FILE"
-if [ $? -ne 0 ]; then
-  echo "Error: Failed to update resolver in $FILE"
-  exit 1
-fi
 
+echo "Executing stack build..."
+stack build
 echo "stack.yaml updated"
