302d841611d90d38cb468b0864731c4dc8f5aedc
Lucas David Traverso <lucas6246@gmail.com>
2026-03-21 01:15:40 -0300

    Agregar dependencia para preview de videos de yt y otros

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
-import starlightImageZoom from 'starlight-image-zoom'
-import starlightLinksValidator from 'starlight-links-validator'
+import starlightImageZoom from 'starlight-image-zoom';
+import starlightLinksValidator from 'starlight-links-validator';
 
 // https://astro.build/config
 export default defineConfig({
